deca881f11bab6a56ff9724522467d9db998efc3
JustXale <alexev2401@gmail.com>
2023-01-24 15:52:51 +0300

    fixes (real this time)

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
 import './css/App.css'
+import './css/media.css'
 import {linktree} from "./consts/links";
 
 function App() {
     return (
-      <div className={'linktree'}>
-          <div className={'profile'}>
-              <img src={'https://cdn.justxale.com/png/profile/avatar.png'} id={'avatar'}/> <p id={'profile-name'}> JustXale</p>
-          </div>
-          <div className={'profile-desc'}>
-              <p id={'newline'}>Just another fluffy creator</p>
-          </div>
-
-          <div>
-              <ul className={'links'}>
-                  {linktree.map((tab, i) => (
-                      <li key={i}>
-                          <a href={tab.link} target={'_blank'}><img src={tab.icon_url}
-                                                                    id={'icon'}
-                                                                    style={tab.style}/> {tab.title}</a> {/*IDK WHY THIS THING DOESN'T WORK*/}
-                          <p id={'description'}>{tab.desc}</p>
-                      </li>
-                  ))}
-              </ul>
-          </div>
-      </div>
-
+        <div className={'linktree'}>
+            <div className={'profile'}>
+                <img src={'https://cdn.justxale.com/png/profile/avatar.png'} id={'avatar'}/> <p id={'profile-name'}> JustXale</p>
+            </div>
+            <div className={'profile-desc'}>
+                <p>Just another fluffy creator</p>
+            </div>
+            <div>
+                <ul className={'links'}>
+                    {linktree.map((tab, i) => (
+                        <li key={i}>
+                            <a href={tab.link} target={'_blank'}><img src={tab.icon_url}
+                                                                      className={tab.class}/> {tab.title}</a>
+                            <p id={'description'}>{tab.desc}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
   )
 }
 
--- src/consts/links.ts
+++ src/consts/links.ts
@@ -3,38 +3,28 @@ export const linktree = [
         title: 'GitHub',
         desc: 'All my projects are stored here!',
         link: 'https://github.com/JustXale',
-        style: {
-            height: '64px',
-            position: "relative",
-            bottom: '-16px'
-        },
+        class: 'icon github',
         icon_url: 'https://cdn.justxale.com/svg/icons/GitHub.svg'
     },
     {
         title: 'Discord',
         desc: 'Want to talk with me or others?',
         link: 'https://discord.gg/N4RRvrTQkc',
-        style: {
-            height: '48px'
-        },
+        class: 'icon',
         icon_url: 'https://cdn.justxale.com/svg/icons/Discord.svg'
     },
     {
         title: 'Telegram',
         desc: 'Want to know anything anout my life?',
         link: 'https://t.me/justficial',
-        style: {
-            height: '48px'
-        },
+        class: 'icon',
         icon_url: 'https://cdn.justxale.com/svg/icons/Telegram.svg'
     },
     {
         title: 'YouTube',
         desc: 'My (empty) YouTube channel',
         link: '',
-        style: {
-            height: '48px'
-        },
+        class: 'icon',
         icon_url: 'https://cdn.justxale.com/svg/icons/YouTube.svg'
     },
 ]
